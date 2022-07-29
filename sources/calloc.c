1ac9c26022d52f6a970532c8b0f81216f5abaf36
Ludovic MENTHILLER <lumenthi@e1r10p18.clusters.42paris.fr>
2022-07-29 19:46:13 +0200

    :bug: Fix thread lock with calloc

--- sources/calloc.c
+++ sources/calloc.c
@@ -6,7 +6,7 @@
 /*   By: lumenthi <lumenthi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/13 19:09:42 by lumenthi          #+#    #+#             */
-/*   Updated: 2022/07/27 12:37:29 by lumenthi         ###   ########.fr       */
+/*   Updated: 2022/07/29 19:44:11 by lumenthi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ void	*calloc(size_t nelem, size_t size) {
 	pthread_mutex_lock(&g_malloc_mutex);
 	void *ret = NULL;
 
+	pthread_mutex_unlock(&g_malloc_mutex);
 	ret = malloc(nelem * size);
+	pthread_mutex_lock(&g_malloc_mutex);
 	if (ret) {
 		ft_bzero(ret, nelem * size);
 	}
